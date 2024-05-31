7ecb453206da647c0701887b6ff0876ae3e545a3
Meanhor.Kea <meanhor1904@gmail.com>
2024-05-31 22:52:25 +0700

    Shift from Finance to Landlord Management system

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,49 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 function Navbar() {
   const [nav, setNav] = useState(false);
+
   const handleNav = () => {
     setNav(!nav);
   };
+
+  // useEffect hook to handle screen resize
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setNav(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white ">
       <h1 className="w-full text-2xl font-bold text-[#00df9a]">
-        Finance Management
+        TenantTrack
       </h1>
       <ul className="hidden md:flex">
-        <li className="p-4">Home</li>
-        <li className="p-4">Company</li>
-        <li className="p-4">Resource</li>
-        <li className="p-4">About</li>
-        <li className="p-4">Contact</li>
+        <li className="p-5">Home</li>
+        <li className="p-5">Features</li>
+        <li className="p-5">Pricing</li>
+        <li className="p-5">Story</li>
+        <li className="p-5">Contact</li>
       </ul>
 
       <div onClick={handleNav} className="block md:hidden">
-        {!nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+        {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
       </div>
 
       <div
         className={
-          !nav
+          nav
             ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
             : "fixed left-[-100%]"
         }
@@ -35,9 +53,9 @@ function Navbar() {
         </h1>
         <ul className="uppercase ">
           <li className="p-4 border-b border-gray-600">Home</li>
-          <li className="p-4 border-b border-gray-600">Company</li>
-          <li className="p-4 border-b border-gray-600">Resource</li>
-          <li className="p-4 border-b border-gray-600">About</li>
+          <li className="p-4 border-b border-gray-600">Feature</li>
+          <li className="p-4 border-b border-gray-600">Pricing</li>
+          <li className="p-4 border-b border-gray-600">Story</li>
           <li className="p-4">Contact</li>
         </ul>
       </div>
