0dcc9b5540b6bceadd41b59f7d6826e81b150a78
Meanhor.Kea <meanhor1904@gmail.com>
2024-06-02 23:01:23 +0700

    update every section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Analytics from "./components/Analytics";
+import Newletter from "./components/Newletter";
+import Feature from "./components/Feature";
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-      <Navbar/>
-    </div>
-  )
+      <Navbar />
+      <Hero />
+      <Analytics />
+      <Feature />
+      <Pricing />
+      <Newletter />
+      <Footer />
+    </div> 
+  );
 }
 
-export default App
+export default App;
